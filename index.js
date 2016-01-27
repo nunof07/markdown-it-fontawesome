66cd6d9baa913203fa915365601f9c2bd5da6d24
Nuno Freitas <nunofreitas@gmail.com>
2016-01-27 08:45:57 +0000

    Fixes #1
    Adds support for icons with hyphens in the name like fa-pause-circle,
    fa-pause-circle-o, etc.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Plugin = require('markdown-it-regexp');
 
 module.exports = function fontawesome_plugin(md) {
 	md.use(Plugin(
-		/\:fa-(\w+)\:/,
+		/\:fa-([\w\-]+)\:/,
 		function (match, utils) {
 			return '<i class="fa fa-' + utils.escape(match[1]) + '"></i>';
 		}
